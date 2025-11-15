fd094a546f2207ab2d9b7df9b5bd227834066017
a1regg <gtkachenko7@gmail.com>
2025-11-15 01:38:10 +0000

    add version to __init__.py

--- attnax/__init__.py
+++ attnax/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
-"""Attnax: Composable attention and transformer components for JAX."""
+"""Attnax: Composable transformer components for JAX."""
+
+__version__ = "0.1.0"
 
 from .attention import MultiHeadAttentionLayer
 from .blocks import DecoderBlock, EncoderBlock
@@ -11,14 +13,15 @@ from .feedforward import FeedForward
 from .masking import combine_masks, make_causal_mask, make_padding_mask
 
 __all__ = [
+    "__version__",
     "MultiHeadAttentionLayer",
-    "TransformerEncoder",
-    "TransformerConfig",
-    "TokenEmbedding",
-    "PositionalEncoding",
-    "FeedForward",
     "EncoderBlock",
     "DecoderBlock",
+    "TransformerEncoder",
+    "FeedForward",
+    "TokenEmbedding",
+    "PositionalEncoding",
+    "TransformerConfig",
     "make_padding_mask",
     "make_causal_mask",
     "combine_masks",
